44bb27040662443e24c5c53978a868caa80d0432
Milkyway Galaxy <blannkisblank@gmail.com>
2025-05-04 21:36:14 +0700

    Update app.py

--- Server/app.py
+++ Server/app.py
@@ -47,5 +47,16 @@ def upload_handler():
     return jsonify({'success': False, 'error': 'Invalid request method'}), 400
 
 
+@app.route('/classify_image', methods=['GET'])
+def classify_image():
+    image_data = request.form['image_data']
+
+    response = jsonify(util.classify_image(image_data))
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+
 if __name__ == "__main__":
     app.run(debug=True)
