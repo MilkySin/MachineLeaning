cba022a77f846ff8dab6bf081db1dfb0fc5b6768
Milkyway Galaxy <blannkisblank@gmail.com>
2025-05-14 23:41:49 +0700

    update

--- Server/util.py
+++ Server/util.py
@@ -1,9 +1,111 @@
 from PIL import Image  # Import Pillow for image processing
 import numpy as np
-#normalize the image
+from flask import jsonify
+import json
+import time
+import tensorflow as tf
+
+# Get absolute paths
+from pathlib import Path
+import os
+
+
+class_labels = [
+    "bacterial_leaf_blight",
+    "bacterial_leaf_streak",
+    "bacterial_panicle_blight",
+    "blast",
+    "brown_spot",
+    "dead_heart",
+    "downy_mildew",
+    "hispa",
+    "normal",
+    "tungro",
+]
+
+BASE_DIR = Path(__file__).resolve().parent
+
+# UI folder is sibling to Server
+UI_DIR = os.path.join(BASE_DIR.parent, "UI")
+
+# Uploads and models are inside Server
+UPLOAD_DIR = os.path.join(BASE_DIR, "uploads")
+PIC_DIR = os.path.join(UPLOAD_DIR, "pic")
+
+
+def loader(model_name, model_path):
+    print(f"Loading {model_name} model...")
+    start_time = time.time()
+    model = tf.keras.models.load_model(model_path)
+    print(f"{model_name} model loaded in {time.time() - start_time:.2f} seconds.")
+    return model
+
+
+# Load models
+model_vgg = loader("VGG", "models/task1_vgg_model.h5")
+model_mobile_net = loader("MobileNet", "models/task1_mobile_net_model.h5")
+model_cnn = loader("CNN", "models/task1_cnn_model.h5")
+
+# normalize the image
+
+
 def preprocess_image(img_path):
     img = Image.open(img_path)
     img = img.resize((224, 224))  # Resize to the target size
     img_array = np.array(img) / 255.0  # Normalize to [0, 1]
     img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
     return img_array
+
+
+def classify_image(file, file_path):
+    # Predict with all models
+    img_array = preprocess_image(file_path)
+
+    try:
+        vgg_prediction = model_vgg.predict(img_array)
+        mobile_net_prediction = model_mobile_net.predict(img_array)
+        cnn_prediction = model_cnn.predict(img_array)
+
+        vgg_index = int(np.argmax(vgg_prediction, axis=1)[0])
+        vgg_confidence = float(np.max(vgg_prediction, axis=1)[0])
+        vgg_label = class_labels[vgg_index]
+
+        mobile_index = int(np.argmax(mobile_net_prediction, axis=1)[0])
+        mobile_confidence = float(np.max(mobile_net_prediction, axis=1)[0])
+        mobile_label = class_labels[mobile_index]
+
+        cnn_index = int(np.argmax(cnn_prediction, axis=1)[0])
+        cnn_confidence = float(np.max(cnn_prediction, axis=1)[0])
+        cnn_label = class_labels[cnn_index]
+
+        predictions = {
+            "vgg": {
+                "label": vgg_label,
+                "confidence": f"{vgg_confidence * 100:.2f}%",
+            },
+            "mobile_net": {
+                "label": mobile_label,
+                "confidence": f"{mobile_confidence * 100:.2f}%",
+            },
+            "cnn": {
+                "label": cnn_label,
+                "confidence": f"{cnn_confidence * 100:.2f}%",
+            },
+        }
+
+        # Save to predictions.json
+        prediction_file_path = os.path.join(UPLOAD_DIR, "predictions.json")
+        data = {file.filename: predictions}
+
+        with open(prediction_file_path, "w") as f:
+            json.dump(data, f, indent=4)
+
+        return jsonify(
+            {
+                "predictions": predictions,
+                "message": "Image classified and predictions saved successfully",
+            }
+        )
+    except Exception as e:
+        print(f"Error during classification: {e}")
+        return jsonify({"success": False, "error": f"Prediction failed"}), 500
--- UI/static/js/app.js
+++ UI/static/js/app.js
@@ -2,144 +2,126 @@
 Dropzone.autoDiscover = false;
 
 const myDropzone = new Dropzone("#dropzone", {
-  url: "/upload-handler",
-  paramName: "file",
-  maxFilesize: 10, // MB
-  acceptedFiles: "image/png, image/jpeg",
-  maxFiles: 1,
-  clickable: true,
-  addRemoveLinks: true,
-  dictRemoveFile: "Remove",
-  dictInvalidFileType: "Only PNG and JPG images are allowed",
-  autoProcessQueue: false,
-  init: function () {
-    const uploadImageElement = document.getElementById("upload-image");
-    const uploadTextImageElement = document.getElementById("upload-image-text");
-    const errorElement = document.getElementById("error"); // Get the error element
+    url: "/upload-handler",
+    paramName: "file",
+    maxFilesize: 10, // MB
+    acceptedFiles: "image/png, image/jpeg",
+    maxFiles: 1,
+    clickable: true,
+    addRemoveLinks: true,
+    dictRemoveFile: "Remove",
+    dictInvalidFileType: "Only PNG and JPG images are allowed",
+    autoProcessQueue: false,
+    init: function () {
+        const uploadImageElement = document.getElementById("upload-image");
+        const uploadTextImageElement = document.getElementById("upload-image-text");
+        const errorElement = document.getElementById("error"); // Get the error element
 
-    this.on("addedfile", function (file) {
-      if (this.files.length === 1) {
-        uploadImageElement.style.display = "none";
-        uploadTextImageElement.style.display = "none";
-      }
-    });
+        this.on("addedfile", function (file) {
+            if (this.files.length === 1) {
+                uploadImageElement.style.display = "none";
+                uploadTextImageElement.style.display = "none";
+            }
+        });
 
-    this.on("removedfile", function () {
-      if (this.files.length === 0) {
-        uploadImageElement.style.display = "block";
-        uploadTextImageElement.style.display = "block";
-        errorElement.style.display = "none";
-      }
-    });
+        this.on("removedfile", function () {
+            if (this.files.length === 0) {
+                uploadImageElement.style.display = "block";
+                uploadTextImageElement.style.display = "block";
+                errorElement.style.display = "none";
+            }
+        });
 
-    this.on("success", function (file, response) {
-      console.log("File uploaded successfully");
-    });
+        this.on("success", function (file, response) {
+            console.log("File uploaded successfully");
+        });
 
-    this.on("error", function (file, errorMessage) {
-      console.error(errorMessage);
-    });
-  },
+        this.on("error", function (file, errorMessage) {
+            console.error(errorMessage);
+        });
+    },
 });
 
 // Add event listener to the Classify button
 document.getElementById("submitBtn").addEventListener("click", function () {
-  const errorElement = document.getElementById("error");
-  errorElement.style.display = "none";
+    const errorElement = document.getElementById("error");
+    errorElement.style.display = "none";
 
-  if (myDropzone.getAcceptedFiles().length === 0) {
-    errorElement.textContent = "Please upload an image before classifying.";
+    if (myDropzone.getAcceptedFiles().length === 0) {
+        errorElement.textContent = "Please upload an image before classifying.";
+        errorElement.style.display = "block";
+        return;
+    }
+    errorElement.textContent = "Classifying image...";
     errorElement.style.display = "block";
-    return;
-  }
-  errorElement.textContent = "Classifying image...";
-  errorElement.style.display = "block";
-  myDropzone.processQueue();
+    myDropzone.processQueue();
 });
 
 myDropzone.on("success", function (file, response) {
-  const errorElement = document.getElementById("error");
-  const classTable = document.getElementById("classTable");
+    const errorElement = document.getElementById("error");
+    const classTable = document.getElementById("classTable");
 
-  // Now trigger classification
-  const formData = new FormData();
-  formData.append("file", file);
+    // Now trigger classification
+    const formData = new FormData();
+    formData.append("file", file);
 
-  fetch("/classify_image", {
-    method: "POST",
-    body: formData,
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      if (data.error) {
-        errorElement.textContent = data.error;
-        errorElement.style.display = "block";
-        classTable.style.display = "none";
-        return;
-      }
-
-      document.getElementById("vgg-label").textContent =
-        data.predictions.vgg.label;
-      document.getElementById("vgg-confidence").textContent =
-        data.predictions.vgg.confidence;
-
-      document.getElementById("mobile-label").textContent =
-        data.predictions.mobile_net.label;
-      document.getElementById("mobile-confidence").textContent =
-        data.predictions.mobile_net.confidence;
-
-      document.getElementById("cnn-label").textContent =
-        data.predictions.cnn.label;
-      document.getElementById("cnn-confidence").textContent =
-        data.predictions.cnn.confidence;
-
-      errorElement.style.display = "none";
-      classTable.style.display = "table";
+    fetch("/classify_image", {
+        method: "POST",
+        body: formData,
     })
-    .catch((err) => {
-      errorElement.textContent = "Classification failed: " + err;
-      errorElement.style.display = "block";
-      classTable.style.display = "none";
-    });
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.error) {
+                errorElement.textContent = data.error;
+                errorElement.style.display = "block";
+                classTable.style.display = "none";
+                return;
+            }
+
+            document.getElementById("vgg-label").textContent = data.predictions.vgg.label;
+            document.getElementById("vgg-confidence").textContent = data.predictions.vgg.confidence;
+
+            document.getElementById("mobile-label").textContent = data.predictions.mobile_net.label;
+            document.getElementById("mobile-confidence").textContent = data.predictions.mobile_net.confidence;
+
+            document.getElementById("cnn-label").textContent = data.predictions.cnn.label;
+            document.getElementById("cnn-confidence").textContent = data.predictions.cnn.confidence;
+
+            errorElement.style.display = "none";
+            classTable.style.display = "table";
+        })
+        .catch((err) => {
+            errorElement.textContent = "Classification failed: " + err;
+            errorElement.style.display = "block";
+            classTable.style.display = "none";
+        });
 });
 
 let currentMode = "class";
 
-document
-  .getElementById("link-classify-disease")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
+document.getElementById("link-classify-disease").addEventListener("click", function (e) {
     currentMode = "class";
-    document.getElementById("prediction-type-header").textContent =
-      "Predicted Disease";
+    document.getElementById("prediction-type-header").textContent = "Predicted Disease";
     clearTable();
-  });
+});
 
-document
-  .getElementById("link-classify-age")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
+document.getElementById("link-classify-age").addEventListener("click", function (e) {
     currentMode = "age";
-    document.getElementById("prediction-type-header").textContent =
-      "Predicted Age";
+    document.getElementById("prediction-type-header").textContent = "Predicted Age";
     clearTable();
-  });
+});
 
-document
-  .getElementById("link-classify-variety")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
+document.getElementById("link-classify-variety").addEventListener("click", function (e) {
     currentMode = "variety";
-    document.getElementById("prediction-type-header").textContent =
-      "Predicted Variety";
+    document.getElementById("prediction-type-header").textContent = "Predicted Variety";
     clearTable();
-  });
+});
 
 function clearTable() {
-  document.getElementById("vgg-label").textContent = "-";
-  document.getElementById("vgg-confidence").textContent = "-";
-  document.getElementById("mobile-label").textContent = "-";
-  document.getElementById("mobile-confidence").textContent = "-";
-  document.getElementById("cnn-label").textContent = "-";
-  document.getElementById("cnn-confidence").textContent = "-";
+    document.getElementById("vgg-label").textContent = "-";
+    document.getElementById("vgg-confidence").textContent = "-";
+    document.getElementById("mobile-label").textContent = "-";
+    document.getElementById("mobile-confidence").textContent = "-";
+    document.getElementById("cnn-label").textContent = "-";
+    document.getElementById("cnn-confidence").textContent = "-";
 }
